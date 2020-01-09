58dbf17b1730cae02292f11b89a7384a156f0c9f
sptkl <caobaiyue@gmail.com>
2020-01-08 22:45:23 -0500

    update

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app.layout = html.Div(children=[
     html.H6('version3'),
     dcc.Dropdown(id='v3',
                 options=versions_options,
-                value='19v2_w_corrections'),
+                value='20v1'),
     dcc.RadioItems(
                 options=[
                     {'label': 'Condo', 'value': 'TRUE'},
@@ -81,14 +81,14 @@ def create_mismatch(v1, v2, v3, condo):
             }
         )
 
-@app.callback(Output('aggregate-area', 'children'),
+@app.callback(Output('null-area', 'children'),
               [Input('v1', 'value'),
               Input('v2', 'value'),
               Input('v3', 'value'),
               Input('condo', 'value')])
 def create_null(v1, v2, v3, condo):
     v1 = requests.post(f'{base_url}/pluto-qaqc/null', 
-            data=json.dumps({"v":v2, "condo":condo , "cached":"FALSE"})).json()['result'][0]
+            data=json.dumps({"v":v1, "condo":condo , "cached":"FALSE"})).json()['result'][0]
 
     v2 = requests.post(f'{base_url}/pluto-qaqc/null', 
             data=json.dumps({'v':v2, "condo": condo, "cached":"FALSE"})).json()['result'][0]
@@ -96,10 +96,61 @@ def create_null(v1, v2, v3, condo):
     v3 = requests.post(f'{base_url}/pluto-qaqc/null', 
             data=json.dumps({'v':v3, "condo": condo, "cached":"FALSE"})).json()['result'][0] 
 
+    v1_name = v1.pop('v')
+    v2_name = v2.pop('v')
+    v3_name = v3.pop('v')
+    v1.pop('condo')
+    v2.pop('condo')
+    v3.pop('condo')
+    total1 = v1.pop('total')
+    total2 = v2.pop('total')
+    total3 = v3.pop('total')
+
+    def generate_graph(v1, v2, total1, total2, v1_name, v2_name):
+        y1 = list(v1.values())
+        y2 = list(v2.values())
+        x = list(v1.keys())
+        y = []
+        for a,b in zip(y1, y2): 
+            if b*total1 == 0:
+                y.append(0)
+            else: 
+                y.append((a*total2)/(b*total1) - 1)
+        return {'x': x, 'y': y, 'type': 'line', 'name': f'{v1_name} - {v2_name}'}
+
+    v1v2 = generate_graph(v1, v2, total1, total2, v1_name, v2_name)
+    v2v3 = generate_graph(v2, v3, total2, total3, v2_name, v3_name)
+
+    return dcc.Graph(
+            figure={
+                'data': [
+                    v1v2,
+                    v2v3
+                ],
+                'layout': {
+                    'title': 'null graph'
+                }
+            }
+        )
+
+@app.callback(Output('aggregate-area', 'children'),
+              [Input('v1', 'value'),
+              Input('v2', 'value'),
+              Input('v3', 'value'),
+              Input('condo', 'value')])
+def create_aggregate(v1, v2, v3, condo):
+    v1 = requests.post(f'{base_url}/pluto-qaqc/aggregate', 
+            data=json.dumps({"v":v1, "condo":condo , "cached":"FALSE"})).json()['result'][0]
+
+    v2 = requests.post(f'{base_url}/pluto-qaqc/aggregate', 
+            data=json.dumps({'v':v2, "condo": condo, "cached":"FALSE"})).json()['result'][0]
+    
+    v3 = requests.post(f'{base_url}/pluto-qaqc/aggregate', 
+            data=json.dumps({'v':v3, "condo": condo, "cached":"FALSE"})).json()['result'][0] 
+
     def generate_graph(r):
         v = r.pop('v')
         r.pop('condo')
-        total = r.pop('total')
         y = list(r.values())
         x = list(r.keys())
         return {'x': x, 'y': y, 'type': 'line', 'name': v}
@@ -116,11 +167,10 @@ def create_null(v1, v2, v3, condo):
                     _v3,
                 ],
                 'layout': {
-                    'title': 'Null graph'
+                    'title': 'agg graph'
                 }
             }
         )
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
